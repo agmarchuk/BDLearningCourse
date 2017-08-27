b24d88596142edd2c66a57466fd2ed49b549c808
agmarchuk <mag@iis.nsk.su>
2017-08-27 10:18:29 +0700

    Начальное наполнение репозитория

---
+++ src/Task01_HelloBigData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task01_HelloBigData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
