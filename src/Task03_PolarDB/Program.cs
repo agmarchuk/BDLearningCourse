6eb348b7d5bc16042e2b9dc787f5e2978797ed7e
agmarchuk <mag@iis.nsk.su>
2017-12-12 05:27:26 +0700

    Начато задание 03

---
+++ src/Task03_PolarDB/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task03_PolarDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            double[] arr = { 1, 2, 3, 99 };
+            Console.WriteLine(arr[3]);
+        }
+    }
+}
