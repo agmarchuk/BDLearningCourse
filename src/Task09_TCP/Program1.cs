191614b4c12197ad6dc3d1111aafeeb2b1f60f60
Alexander G. Marchuk <mag@iis.nsk.su>
2018-05-06 05:53:36 +0700

    Работаю над TCP клиентом

--- src/Task09_TCP/Program1.cs
+++ src/Task09_TCP/Program1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
@@ -10,10 +11,30 @@ namespace Task09_TCP
     {
         private static IPAddress host = IPAddress.Parse("127.0.0.1");
         private static int port = 5000;
+        private static System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
         static void Main(string[] args)
         {
-            Console.WriteLine("Start TCP Server");
-            Server();
+            if (args.Any(s => s == "client"))
+            {
+                Console.WriteLine("Start TCP Client");
+                Client();
+            }
+            else if (args.Any(s => s == "clients"))
+            {
+                Console.WriteLine("Start TCP Clients");
+                sw.Restart();
+                for (int i=0; i<1000; i++)
+                {
+                    Client();
+                }
+                sw.Stop();
+                Console.WriteLine($"duration={sw.ElapsedMilliseconds}");
+            }
+            else
+            {
+                Console.WriteLine("Start TCP Server");
+                Server();
+            }
         }
         static void Server()
         {
@@ -22,23 +43,17 @@ namespace Task09_TCP
             while(true)
             {
                 var client = listener.AcceptTcpClient();
-                Console.WriteLine("client accepted");
+                //Console.WriteLine("client accepted");
                 var stream = client.GetStream();
                 byte[] buff = new byte[1000];
 
                 // Принимаем
                 int nbytes = stream.Read(buff, 0, buff.Length);
                 string received = System.Text.Encoding.UTF8.GetString(buff, 0, nbytes);
-                Console.WriteLine(received);
-                if (received == "exit") break;
+                //Console.WriteLine(received);
 
                 // Посылаем
-                //string message = "OK " + DateTime.Now.ToString();
-                string message = @"HTTP/1.0 200 OK
-Content-Type: text/plain
-Content-Length: 6
-
-Hello!";
+                string message = "OK";
                 byte[] arr = System.Text.Encoding.ASCII.GetBytes(message);
                 stream.Write(arr, 0, arr.Length);
 
@@ -46,6 +61,26 @@ Hello!";
                 client.Close();
             }
             listener.Stop();
+        }
+        static void Client()
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(host, port);
+            //Console.WriteLine("client connected");
+
+            var stream = client.GetStream();
+            byte[] buff = new byte[1000];
+
+            // Посылаем
+            byte[] arr = System.Text.Encoding.ASCII.GetBytes("request");
+            stream.Write(arr, 0, arr.Length);
+
+            // Принимаем
+            int nbytes = stream.Read(buff, 0, 3);// buff.Length);
+            string received = System.Text.Encoding.UTF8.GetString(buff, 0, nbytes);
+            //Console.WriteLine(received);
+
+            client.Close();
         }
     }
 }
