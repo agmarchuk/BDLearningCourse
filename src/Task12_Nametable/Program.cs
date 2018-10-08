f20f2e3fce6714a6142059da0276f1a2cfbd2a40
agmarchuk <mag@iis.nsk.su>
2018-10-09 00:41:27 +0700

    НАчал работать над 12-м проектом

---
+++ src/Task12_Nametable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Polar.DB;
+
+namespace Task12_Nametable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Task12_Nametable");
+            PType tp_rec = new PTypeRecord(
+                new NamedType("key", new PType(PTypeEnumeration.sstring)),
+                new NamedType("value", new PType(PTypeEnumeration.integer)));
+            int nelements = 1_000_000;
+
+
+        }
+    }
+}
