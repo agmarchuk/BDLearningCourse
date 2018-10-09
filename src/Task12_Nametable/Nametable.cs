fa6d3e59b1cd42a2446b9faeaec63611feea8853
agmarchuk <mag@iis.nsk.su>
2018-10-09 10:49:18 +0700

    Сделал 12-е задание на таблицу имен

---
+++ src/Task12_Nametable/Nametable.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Task12_Nametable
+{
+    public class Nametable
+    {
+        private FileStream fs;
+        private BinaryWriter bw;
+        private BinaryReader br;
+        private int nelements;
+        private long freespace;
+        private Dictionary<string, int> dic = new Dictionary<string, int>();
+        public Nametable(string file)
+        {
+            fs = File.Open(file, FileMode.OpenOrCreate);
+            bw = new BinaryWriter(fs);
+            br = new BinaryReader(fs);
+            // файл может быть пустой или уже заполненный
+            if (fs.Length == 0L)
+            {
+                nelements = 0;
+                freespace = 8L;
+                bw.Write(0L);
+            }
+            else
+            {
+                fs.Position = 0L;
+                nelements = (int)br.ReadInt64();
+                for (int i=0; i<nelements; i++)
+                {
+                    string key = br.ReadString();
+                    int value = br.ReadInt32();
+                    if (!dic.ContainsKey(key))
+                    {
+                        dic.Add(key, value);
+                    }
+                }
+                freespace = fs.Position;
+            }
+            Flush();
+        }
+        public void Add(string key, int value)
+        {
+            if (!dic.ContainsKey(key))
+            {
+                dic.Add(key, value);
+                fs.Position = freespace;
+                bw.Write(key);
+                bw.Write(value);
+                freespace = fs.Position;
+                nelements += 1;
+            }
+        }
+        public void Flush()
+        {
+            fs.Position = 0L;
+            bw.Write((long)nelements);
+            bw.Flush(); fs.Flush();
+        }
+        public int GetByKey(string key)
+        {
+            if (dic.TryGetValue(key, out int value)) return value;
+            return Int32.MinValue;
+        }
+    }
+}
--- src/Task12_Nametable/Program.cs
+++ src/Task12_Nametable/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 using Polar.DB;
 
 namespace Task12_Nametable
@@ -8,11 +11,66 @@ namespace Task12_Nametable
         static void Main(string[] args)
         {
             Console.WriteLine("Start Task12_Nametable");
-            PType tp_rec = new PTypeRecord(
-                new NamedType("key", new PType(PTypeEnumeration.sstring)),
-                new NamedType("value", new PType(PTypeEnumeration.integer)));
             int nelements = 1_000_000;
 
+            // Генератор тестовых данных
+            Func<int, IEnumerable<object[]>> Generate = n => Enumerable.Range(0, n).Select(i => new object[] { i.ToString(), i });
+
+            // Проверим его работу
+            foreach (object[] pair in Generate(10))
+            {
+                Console.WriteLine($"{pair[0].GetType().Name} {pair[0]} {pair[1].GetType().Name} {pair[1]} ");
+            }
+
+            // Файл и его заполнение
+            FileStream fs = File.Open("data.bin", FileMode.OpenOrCreate);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write((long)nelements);
+            Generate(nelements).Select(pair => { bw.Write((string)pair[0]); bw.Write((int)pair[1]); return true; }).Count();
+            bw.Flush(); fs.Flush();
+
+            // Сканирование файла с выдачей последних элементов
+            BinaryReader br = new BinaryReader(fs);
+            fs.Position = 0L;
+            int ne = (int)br.ReadInt64();
+            Console.WriteLine(ne);
+            var query = Enumerable.Repeat(0, ne).Select(i =>
+            {
+                object[] pa = new object[2];
+                pa[0] = br.ReadString();
+                pa[1] = br.ReadInt32();
+                return pa;
+            });
+            foreach (object[] pa in query.TakeLast(10))
+            {
+                Console.WriteLine($"element: {pa[0]} {pa[1]}");
+            }
+
+            fs.Position = 0L;
+
+            Console.WriteLine("==== use Nametable class ====");
+            Nametable nt = new Nametable("nametable.bin");
+            foreach (object[] pa in Generate(nelements))
+            {
+                //Console.WriteLine($"element: {pa[0]} {pa[1]}");
+                nt.Add((string)pa[0], (int)pa[1]);
+            }
+            nt.Flush();
+
+            string k = (nelements * 2 / 3).ToString();
+            Console.WriteLine($" key={k} value={nt.GetByKey(k)}");
+
+            // значение состоит из имени и списка ключей друзей
+            PType tp_rec = new PTypeRecord(
+                new NamedType("key", new PType(PTypeEnumeration.sstring)),
+                new NamedType("value",
+                    new PTypeRecord(
+                        new NamedType("name", new PType(PTypeEnumeration.sstring)),
+                        new NamedType("friends", new PTypeSequence(new PType(PTypeEnumeration.sstring))))));
+
+            // Сериализация и десериализация
+            //ByteFlow.Serialize(bw, v, tp_rec);
+            //ByteFlow.Deserialize(br, tp_rec);
 
         }
     }
